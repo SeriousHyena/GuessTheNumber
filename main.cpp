3db47c67cca901c860bcb63d4421ab88dc53fa4b
SeriousHyena <emackiii@gmail.com>
2021-10-07 14:44:05 -0700

    progress

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+/*
+
+*/
+
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -6,6 +10,20 @@ using namespace std;
 
 int main()
 {
+	int counter = 0;
+	int randomNumber = 0;
+	srand(time(nullptr));
+
+	for (int i = 0; i < 100; i++)
+	{
+		randomNumber = rand() % 100;
+		cout << randomNumber << endl;
+		counter++;
+		
+	}
+	cout << "We processed " << counter << " numbers." << endl;
 
 	return 0;
 }
+
+// STILL IN PROCESS
